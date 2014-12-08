e12425935881233e031bc7d5c27d6d06a0f30377
Michaël Zasso <mic.besace@gmail.com>
2014-12-08 13:31:32 +0100

    tests

--- src/org/cheminfo/actelion/test/BigmolTest.java
+++ src/org/cheminfo/actelion/test/BigmolTest.java
@@ -6,16 +6,18 @@ public class BigmolTest {
 		
 	public static void test() throws Exception {
 		
-		String idcode = "f\\VIpA~`@@@A`A@hM@XOADLaTN`LC`|@PbLQrIQjKPzDpVFpNIq^OqALHQAHiCHyOIeJiuIhmGhCDXSJYKMY[OXGBxwAxoMyHE`jDpeDXgDDheTneLmd\\`TbbTrnUjkUzhtfftNet^kuAlMQnLIcLy`lelmuimmkl}d\\Sf\\Ki][o]Gl|Wa|oc|o}`ZCpYBhWBDTbTZcL]c\\_bBRRrQRj]SzXsfZrvUr^WrAXKQ^KI]JYPjeRju^km[k}XZcVZKUZ[[[G\\{W^zOSzPF`|GpyGh{FxtfTvfLyg\\gBtWdbbbTRTfJbvRtfTRrVdrbVRtfTRrVdrbVRtfTRrVdrbVRtfTRrVdrbVRtfTRrVdrbVRtfTRrVdrbVRtfTRrVdrbVRtfTRrVdrbVRtfTRrVdrbVRtfTRrVdrbVRtfTRrVdrbVRtfTRrVdrbVRtfTRrVdrbVRtfTRrVdrbVRtfTRrVdrbVRtfTRrVdrbVRtfTRrVdrbVRtfTRrVdrbVu@d@PRFpnCHEDXsAx_GDdbTJet~`lUf\\kc|@TbtQRZWraRjMUZ{PFPvflsVFtNqqn]w^grAHMa|HqVNIiKiCLywIEXoebjuNmMyh]Sn}okCD\\SrYs^]UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUTr`BPDHBhBxBdBlDBDRDZBfDvDnDaBIDYDEDMB}DcDsD{BWDOD_DPbhdxdddlbBdRdJdFbvdnd~dqbYdEdUd]bcdsdkdgbOd_d@THRxTdTtT|RRTJTZTVRnT~TaTiRETUTMTCRsTkT{TwR_T@tPtXrdtttltbrJtZtFtNr~tatqtyrUtMt]tSrkt{tgtor@LPLHLDJtLlL|LrJZLFLVL^JaLqLiLeJML]LCLKJ{LgLwLJPlHlXlTjll|lbljjFlVlNlAjqlilyluj]lClSl[jglwlol`ZH\\X\\D\\LZ|\\b\\r\\zZV\\N\\^\\@@";
+	//	String idcode = "f\\VIpA~`@@@A`A@hM@XOADLaTN`LC`|@PbLQrIQjKPzDpVFpNIq^OqALHQAHiCHyOIeJiuIhmGhCDXSJYKMY[OXGBxwAxoMyHE`jDpeDXgDDheTneLmd\\`TbbTrnUjkUzhtfftNet^kuAlMQnLIcLy`lelmuimmkl}d\\Sf\\Ki][o]Gl|Wa|oc|o}`ZCpYBhWBDTbTZcL]c\\_bBRRrQRj]SzXsfZrvUr^WrAXKQ^KI]JYPjeRju^km[k}XZcVZKUZ[[[G\\{W^zOSzPF`|GpyGh{FxtfTvfLyg\\gBtWdbbbTRTfJbvRtfTRrVdrbVRtfTRrVdrbVRtfTRrVdrbVRtfTRrVdrbVRtfTRrVdrbVRtfTRrVdrbVRtfTRrVdrbVRtfTRrVdrbVRtfTRrVdrbVRtfTRrVdrbVRtfTRrVdrbVRtfTRrVdrbVRtfTRrVdrbVRtfTRrVdrbVRtfTRrVdrbVRtfTRrVdrbVRtfTRrVdrbVRtfTRrVdrbVu@d@PRFpnCHEDXsAx_GDdbTJet~`lUf\\kc|@TbtQRZWraRjMUZ{PFPvflsVFtNqqn]w^grAHMa|HqVNIiKiCLywIEXoebjuNmMyh]Sn}okCD\\SrYs^]UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUTr`BPDHBhBxBdBlDBDRDZBfDvDnDaBIDYDEDMB}DcDsD{BWDOD_DPbhdxdddlbBdRdJdFbvdnd~dqbYdEdUd]bcdsdkdgbOd_d@THRxTdTtT|RRTJTZTVRnT~TaTiRETUTMTCRsTkT{TwR_T@tPtXrdtttltbrJtZtFtNr~tatqtyrUtMt]tSrkt{tgtor@LPLHLDJtLlL|LrJZLFLVL^JaLqLiLeJML]LCLKJ{LgLwLJPlHlXlTjll|lbljjFlVlNlAjqlilyluj]lClSl[jglwlol`ZH\\X\\D\\LZ|\\b\\r\\zZV\\N\\^\\@@";
+		String idcode = "gmhMHx@@D@bpDj]@RKUV`rXnVjuzkPG@ptaXeQYln@bTlwHhdmLhhlhhdhjhhheDd]ihlhhdhdTeEIhfhhXmDhdh\\hcBE@iT^@jWKMv|aVjfjZijfjZBZBJZ`hFbjjfjjZjjfZii``@@";
+		Molecule mol1 = Molecule.fromIDCode(idcode, false);
 		
-		Molecule mol1 = Molecule.fromIDCode(idcode, true);
-		
-		System.out.println(mol1.toSmiles());
+		System.out.println(mol1.getIDCode());
 		
 		
 	}
+	
 
 	public static void main(String[] args) throws Exception {
-		test();	}
+		test();
+	}
 
 }
