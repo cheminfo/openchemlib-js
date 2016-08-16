b430de2fad09d1c3157cb5ca3f58c05ece779635
Michaël Zasso <targos@protonmail.com>
2016-08-16 10:23:38 +0200

    add benchmark [ci skip]

---
+++ benchmark/molfile.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const Benchmark = require('benchmark');
+const OCLNew = require('../dist/openchemlib-core');
+const OCLOld = require('../distold/openchemlib-core');
+
+const idcode = 'enYXNH@MHDAELem`OCIILdhhdiheCDlieKDdefndZRVVjjfjjfjihJBbb@@@';
+const mol = OCLNew.Molecule.fromIDCode(idcode);
+const molfile = mol.toMolfile();
+
+const suite = new Benchmark.Suite;
+
+suite
+    .add('old', function () {
+        OCLNew.Molecule.fromMolfile(molfile);
+    })
+    .add('new', function () {
+        OCLOld.Molecule.fromMolfile(molfile);
+    })
+    .on('cycle', function (event) {
+        console.log(String(event.target));
+    })
+    .on('complete', function () {
+        console.log('Fastest is ' + this.filter('fastest').map('name'));
+    })
+    .run();
