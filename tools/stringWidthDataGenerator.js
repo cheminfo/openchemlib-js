72e492fc61070b0efdacdeb4308fb55c6e70308a
Michaël Zasso <targos@protonmail.com>
2017-01-05 17:05:16 +0100

    compute all widths from 0 to 127

--- tools/stringWidthDataGenerator.js
+++ tools/stringWidthDataGenerator.js
@@ -7,70 +7,15 @@ function getStringWidth(str, size = 20) {
     return text.width;
 }
 
-var data = {};
-
-var a = 'a'.charCodeAt(0);
-for (var i = a; i < a + 26; i++) {
-    var str = String.fromCharCode(i);
-    data[str] = getStringWidth(str);
-}
-
-var A = 'A'.charCodeAt(0);
-for (var i = A; i < A + 26; i++) {
-    var str = String.fromCharCode(i);
-    data[str] = getStringWidth(str);
-}
-
-var z = '0'.charCodeAt(0);
-for (var i = z; i < z + 10; i++) {
-    var str = String.fromCharCode(i);
-    data[str] = getStringWidth(str);
-}
-
-var additionalChars = [
-    ' ',
-    '\'',
-    '"',
-    '<',
-    '>',
-    '%',
-    '#',
-    '!',
-    '?',
-    '^',
-    '$',
-    '&',
-    '|',
-    '_',
-    '-',
-    '{',
-    '}',
-    '(',
-    ')',
-    '[',
-    ']',
-    '/',
-    '\\',
-    '+',
-    '*',
-    '.',
-    '@',
-    '=',
-    '~'
-];
-for (var char of additionalChars) {
-    data[char] = getStringWidth(char);
-}
-
-var result = [];
-for (var char in data) {
-    result[char.charCodeAt(0)] = data[char];
+var result = new Array(128);
+for (var c = 0; c < 128; c++) {
+  result[c] = getStringWidth(String.fromCharCode(c));
 }
 
 var pointer = 0;
 var helveticaSizes = ['private static final double[] helveticaSizes = {'];
 for (var i = 0; i < 8; i++) {
-    var str = '\t';
+    var str = '    ';
     for (var j = 0; j < 16; j++) {
         var value = result[pointer++] || 5.55;
         value = Math.round(value * 100) / 100;
