cbaaf9485efbd0e85276cd2d274463fa8b843127
Luc Patiny <luc@patiny.com>
2017-07-06 17:48:53 +0200

    Remove CIP from DiastereotopicAtomID

--- src/com/actelion/research/gwt/chemlib/com/actelion/research/chem/contrib/DiastereotopicAtomID.java
+++ src/com/actelion/research/gwt/chemlib/com/actelion/research/chem/contrib/DiastereotopicAtomID.java
@@ -58,7 +58,7 @@ public class DiastereotopicAtomID {
 	
 	private static void makeRacemic(StereoMolecule molecule) {
 		// if we don't calculate this we have 2 epimers
-		molecule.ensureHelperArrays(Molecule.cHelperCIP);
+		molecule.ensureHelperArrays(Molecule.cHelperParities);
 		// we need to make one group "AND" for chiral (to force to racemic, this means diastereotopic and not enantiotopic)
 		for (int i=0; i<molecule.getAllAtoms(); i++) {
 			if (molecule.getAtomParity(i)!=Molecule.cAtomParityNone) {
