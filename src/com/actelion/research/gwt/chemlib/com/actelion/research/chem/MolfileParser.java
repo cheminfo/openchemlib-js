848d71649f3998755d37432ad570e9992ab0f41c
Luc Patiny <luc@patiny.com>
2017-07-06 15:45:42 +0200

    update diastereotopicAtomID not to rely on CIP

--- src/com/actelion/research/gwt/chemlib/com/actelion/research/chem/MolfileParser.java
+++ src/com/actelion/research/gwt/chemlib/com/actelion/research/chem/MolfileParser.java
@@ -1,35 +1,20 @@
 /*
-* Copyright (c) 1997 - 2016
-* Actelion Pharmaceuticals Ltd.
-* Gewerbestrasse 16
-* CH-4123 Allschwil, Switzerland
-*
-* All rights reserved.
-*
-* Redistribution and use in source and binary forms, with or without
-* modification, are permitted provided that the following conditions are met:
-*
-* 1. Redistributions of source code must retain the above copyright notice, this
-*    list of conditions and the following disclaimer.
-* 2. Redistributions in binary form must reproduce the above copyright notice,
-*    this list of conditions and the following disclaimer in the documentation
-*    and/or other materials provided with the distribution.
-* 3. Neither the name of the the copyright holder nor the
-*    names of its contributors may be used to endorse or promote products
-*    derived from this software without specific prior written permission.
-*
-* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-* ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
-* ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*
-*/
+ * Copyright 2014 Actelion Pharmaceuticals Ltd., Gewerbestrasse 16, CH-4123 Allschwil, Switzerland
+ *
+ * This file is part of DataWarrior.
+ * 
+ * DataWarrior is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ * 
+ * DataWarrior is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with DataWarrior.
+ * If not, see http://www.gnu.org/licenses/.
+ *
+ * @author Florian Monachon,Thomas Sander,Christian Rufener
+ */
 
 /*
  *       Date            User        Remark
@@ -56,24 +41,44 @@ import java.util.TreeMap;
 
 public class MolfileParser
 {
+	public static final int MODE_KEEP_HYDROGEN_MAP = 1;
+
 	public static boolean debug = false;
 	private StereoMolecule mMol;
 	private TreeMap<Integer,Integer> mAtomIndexMap,mBondIndexMap;
 	private boolean mTreatAnyAsMetalBond,mDeduceMissingCharges;
-
+	private int mMode;
+	private int[] mHydrogenMap;
 
 	/**
 	 * Constructor of a MolFileParser, which will mirror Y,Z coordinates
 	 */
 	public MolfileParser() {
+		mMode = 0;
 	}
-	
+
+
+	public MolfileParser(int mode) {
+		mMode = mode;
+	}
+
+
+	/**
+	 * If this MoflileParser was instantiated with MODE_KEEP_HYDROGEN_MAP
+	 * @return
+	 */
+	public int[] getHandleHydrogenMap() {
+		return mHydrogenMap == null ? mMol.getHandleHydrogenMap() : mHydrogenMap;
+	}
+
 
 	private boolean readMoleculeFromBuffer(BufferedReader reader)
 	{
 		try{
 			String line;
 			int natoms,nbonds,nlists,chiral,version;
+
+			mHydrogenMap = null;
 
 			if(mMol != null){
 				mMol.deleteMolecule();
@@ -247,6 +252,8 @@ public class MolfileParser
 
 				if(chiral == 0){
 					// to run the racemization scheduled with mMol.setToRacemate()
+					if ((mMode & MODE_KEEP_HYDROGEN_MAP) != 0)
+						mHydrogenMap = mMol.getHandleHydrogenMap();
 					mMol.ensureHelperArrays(Molecule.cHelperParities);
 				}
 
@@ -412,6 +419,8 @@ public class MolfileParser
 
 		// needs to be done for molfiles with chiral=0 that have stereo
 		// centers which will be assigned to one ESR-AND group
+		if ((mMode & MODE_KEEP_HYDROGEN_MAP) != 0)
+			mHydrogenMap = mMol.getHandleHydrogenMap();
 		mMol.ensureHelperArrays(Molecule.cHelperParities);
 
 		return true;
--- src/com/actelion/research/gwt/chemlib/com/actelion/research/chem/contrib/DiastereotopicAtomID.java
+++ src/com/actelion/research/gwt/chemlib/com/actelion/research/chem/contrib/DiastereotopicAtomID.java
@@ -13,10 +13,6 @@ public class DiastereotopicAtomID {
 		StereoMolecule tempMolecule;
 		for (int iAtom=0; iAtom<numberAtoms; iAtom++) {
 			tempMolecule=molecule.getCompactCopy();
-			
-			// Temporary bug fix
-			tempMolecule.ensureHelperArrays(ExtendedMolecule.cHelperCIP);
-
 			changeAtom(tempMolecule, iAtom);
 			makeRacemic(tempMolecule);
 			ids[iAtom]=(new Canonizer(tempMolecule, Canonizer.ENCODE_ATOM_CUSTOM_LABELS)).getIDCode();
@@ -32,50 +28,22 @@ public class DiastereotopicAtomID {
 	public static void addMissingChirality(StereoMolecule molecule) {
 		for (int iAtom=0; iAtom<molecule.getAllAtoms(); iAtom++) {
 			StereoMolecule tempMolecule=molecule.getCompactCopy();
-			// after copy we need to recalculate the helpers ...
-			tempMolecule.ensureHelperArrays(Molecule.cHelperCIP);
-			
 			changeAtom(tempMolecule, iAtom);
 			// we need to have >0 and not >1 because there could be unspecified chirality in racemate
-			if (tempMolecule.getStereoCenterCount()>0) {
-				for (int i=0; i<tempMolecule.getAtoms(); i++) {
-
-					// changed from from handling below; TLS 9.Nov.2015
-					if (tempMolecule.isAtomStereoCenter(i) && tempMolecule.getStereoBond(i)==-1) {
-						int stereoBond = tempMolecule.getAtomPreferredStereoBond(i);
-						if (stereoBond != -1) {
-							molecule.setBondType(stereoBond, Molecule.cBondTypeUp);
-							if (molecule.getBondAtom(1, stereoBond) == i) {
-								int connAtom = molecule.getBondAtom(0, stereoBond);
-								molecule.setBondAtom(0, stereoBond, i);
-								molecule.setBondAtom(1, stereoBond, connAtom);
-							}
-						// To me it seems that we have to add all stereo centers into AND group 0.	TLS 9.Nov.2015
-						molecule.setAtomESR(i, Molecule.cESRTypeAnd, 0);
+			for (int i=0; i<tempMolecule.getAtoms(); i++) {
+				// changed from from handling below; TLS 9.Nov.2015
+				if (tempMolecule.isAtomStereoCenter(i) && tempMolecule.getStereoBond(i)==-1) {
+					int stereoBond = tempMolecule.getAtomPreferredStereoBond(i);
+					if (stereoBond != -1) {
+						molecule.setBondType(stereoBond, Molecule.cBondTypeUp);
+						if (molecule.getBondAtom(1, stereoBond) == i) {
+							int connAtom = molecule.getBondAtom(0, stereoBond);
+							molecule.setBondAtom(0, stereoBond, i);
+							molecule.setBondAtom(1, stereoBond, connAtom);
 						}
+					// To me it seems that we have to add all stereo centers into AND group 0.	TLS 9.Nov.2015
+					molecule.setAtomESR(i, Molecule.cESRTypeAnd, 0);
 					}
-
-/*					if ((tempMolecule.getStereoProblem(i)) && (tempMolecule.getStereoBond(i)==-1)) {
-						// we need to add a stereobond
-						// we find a single bond that we could convert ...
-						tempMolecule.ensureHelperArrays(Molecule.cHelperNeighbours);
-						int bondToChange=-1;	// We will prefer to change a bond that is not in the ring !
-				        for (int j=0; j<tempMolecule.getAllConnAtoms(i); j++) {
-				            int bond = tempMolecule.getConnBond(i, j);
-				            if ((tempMolecule.getBondType(bond)==Molecule.cBondTypeSingle) && (tempMolecule.getBondAtom(0,bond)==i)) {
-				            	bondToChange=bond;
-				// TODO: reactivate the change
-				            	if (tempMolecule.getBondRingSize(bond)==0) {
-					            	molecule.changeBond(bond, Molecule.cBondTypeUp);
-					            	bondToChange=-1;
-									break;
-				            	}
-				            }
-				        }
-				        if (bondToChange>-1) {
-				 //       	molecule.changeBond(bondToChange, Molecule.cBondTypeUp);
-				        }
-					}*/
 				}
 			}
 		}
