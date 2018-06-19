5a48d6b2a9b52911d032dc9b838cdc7737b2a99a
Michaël Zasso <targos@protonmail.com>
2018-06-19 15:46:19 +0200

    eslint fix in tools

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,53 @@
 'use strict';
 
-var child_process = require('child_process');
-var gulp = require('gulp');
+var childProcess = require('child_process');
 var path = require('path');
+
+var gulp = require('gulp');
 var fs = require('fs-extra');
 var exporter = require('gwt-api-exporter');
 var rimraf = require('rimraf');
-var pack = require('./package.json');
 var argv = require('minimist')(process.argv.slice(2));
+
+var pack = require('./package.json');
+var modules = require('./modules.json');
 
 var verbose = argv.v;
 
-var modules = require('./modules.json');
-
-var suffix='';
+var suffix = '';
 if (argv.suffix) {
-    suffix='.'+argv.suffix;
+  suffix = `.${argv.suffix}`;
 }
 
-if(argv.m) {
-    for (var i = 0; i < modules.length; i++) {
-        if(modules[i].name === argv.m) {
-            modules = [modules[i]];
-            break;
-        }
+if (argv.m) {
+  for (var i = 0; i < modules.length; i++) {
+    if (modules[i].name === argv.m) {
+      modules = [modules[i]];
+      break;
     }
-    if (modules.length !== 1) {
-        throw new Error('module ' + argv.m + ' not found');
-    }
+  }
+  if (modules.length !== 1) {
+    throw new Error(`module ${argv.m} not found`);
+  }
 }
 
 try {
-    var config = require('./config.json');
+  var config = require('./config.json');
 } catch (e) {
-    console.error('config.json not found. You can copy config.default.json to start from an example.');
-    process.exit(1);
+  // eslint-disable-next-line no-console
+  console.error('config.json not found. You can copy config.default.json to start from an example.');
+  // eslint-disable-next-line no-process-exit
+  process.exit(1);
 }
 
 var classpath = ['src'];
 
 if (config.classpath) {
-    if (typeof config.classpath === 'string') {
-        classpath.push(config.classpath);
-    } else if (Array.isArray(config.classpath)) {
-        classpath = classpath.concat(config.class);
-    }
+  if (typeof config.classpath === 'string') {
+    classpath.push(config.classpath);
+  } else if (Array.isArray(config.classpath)) {
+    classpath = classpath.concat(config.class);
+  }
 }
 
 classpath.push(path.join(config.gwt, 'gwt-dev.jar'), path.join(config.gwt, 'gwt-user.jar'));
@@ -63,109 +66,112 @@ gulp.task('default', ['build:min']);
 gulp.task('copy:openchemlib', copyOpenchemlib);
 
 function build(done) {
-    var prom = [];
-    for (var k = 0; k < modules.length; k++) {
-        var mod = modules[k];
-        log('Exporting module ' + mod.name);
-        var warDir = path.join('war', mod.war);
-        var files = fs.readdirSync(warDir);
-        var file;
-        for (var i = 0; i < files.length; i++) {
-            if (files[i].indexOf('.cache.js') > 0) {
-                file = path.join(warDir, files[i]);
-                break;
-            }
-        }
-        if (!file) {
-            throw new Error('Could not find GWT file for module ' + mod.name);
-        }
-        prom.push(exporter({
-            input: file,
-            output: 'dist/openchemlib-' + mod.name + suffix + '.js',
-            exports: 'OCL',
-            fake: mod.fake,
-            'package': pack
-        }));
+  var prom = [];
+  for (var k = 0; k < modules.length; k++) {
+    var mod = modules[k];
+    log(`Exporting module ${mod.name}`);
+    var warDir = path.join('war', mod.war);
+    var files = fs.readdirSync(warDir);
+    var file;
+    for (var i = 0; i < files.length; i++) {
+      if (files[i].indexOf('.cache.js') > 0) {
+        file = path.join(warDir, files[i]);
+        break;
+      }
     }
-    Promise.all(prom).then(function () {
-        done();
-    }, function (e) {
-        console.error(e);
-        process.exit(1);
-    })
+    if (!file) {
+      throw new Error(`Could not find GWT file for module ${mod.name}`);
+    }
+    prom.push(exporter({
+      input: file,
+      output: `dist/openchemlib-${mod.name}${suffix}.js`,
+      exports: 'OCL',
+      fake: mod.fake,
+      package: pack
+    }));
+  }
+  Promise.all(prom).then(function () {
+    done();
+  }, function (e) {
+    // eslint-disable-next-line no-console
+    console.error(e);
+    // eslint-disable-next-line no-process-exit
+    process.exit(1);
+  });
 }
 
 function compile(mode) {
-    return function () {
-        var min = mode === 'min';
-        for (var i = 0; i < modules.length; i++) {
-            log('Compiling module ' + modules[i].name);
-            var args = [
-                '-Xmx512m',
-                '-cp', classpath,
-                'com.google.gwt.dev.Compiler',
-                modules[i].entrypoint,
-                '-logLevel', verbose ? 'DEBUG' : 'ERROR',
-                min ? '-XnocheckCasts' : '-XcheckCasts',
-                '-XnoclassMetadata',
-                verbose ? '-draftCompile' : '-nodraftCompile',
-                '-nocheckAssertions',
-                '-generateJsInteropExports',
-                '-optimize', min ? '9' : '0',
-                '-style', min ? 'OBFUSCATED' : 'PRETTY',
+  return function () {
+    var min = mode === 'min';
+    for (var i = 0; i < modules.length; i++) {
+      log(`Compiling module ${modules[i].name}`);
+      var args = [
+        '-Xmx512m',
+        '-cp', classpath,
+        'com.google.gwt.dev.Compiler',
+        modules[i].entrypoint,
+        '-logLevel', verbose ? 'DEBUG' : 'ERROR',
+        min ? '-XnocheckCasts' : '-XcheckCasts',
+        '-XnoclassMetadata',
+        verbose ? '-draftCompile' : '-nodraftCompile',
+        '-nocheckAssertions',
+        '-generateJsInteropExports',
+        '-optimize', min ? '9' : '0',
+        '-style', min ? 'OBFUSCATED' : 'PRETTY',
       //          verbose ? '-failOnError' : '-nofailOnError'
-            ];
-            var result;
-            try {
-                result = child_process.execFileSync('java', args);
-            } catch (e) {
-                result = e.stdout;
-                throw e;
-            } finally {
-                if (verbose) {
-                    var name = 'compile-' + modules[i].name + '.log';
-                    log('Compilation log written to ' + name);
-                    fs.writeFileSync('./' + name, result);
-                }
-            }
+      ];
+      var result;
+      try {
+        result = childProcess.execFileSync('java', args);
+      } catch (e) {
+        result = e.stdout;
+        throw e;
+      } finally {
+        if (verbose) {
+          var name = `compile-${modules[i].name}.log`;
+          log(`Compilation log written to ${name}`);
+          fs.writeFileSync(`./${name}`, result);
         }
+      }
     }
+  };
 }
 
 function copyOpenchemlib() {
-    const chemlibDir = path.join(config.openchemlib, 'main/java/com');
-    const outDir = './src/com/actelion/research/gwt/chemlib/com';
-    const modifiedDir = './openchemlib/modified/com';
+  const chemlibDir = path.join(config.openchemlib, 'main/java/com');
+  const outDir = './src/com/actelion/research/gwt/chemlib/com';
+  const modifiedDir = './openchemlib/modified/com';
 
-    const chemlibClasses = require('./openchemlib/classes');
+  const chemlibClasses = require('./openchemlib/classes');
 
-    rimraf.sync(outDir);
+  rimraf.sync(outDir);
 
-    fs.copySync(chemlibDir, outDir);
+  fs.copySync(chemlibDir, outDir);
 
-    const modified = chemlibClasses.modified;
-    log('Copying ' + modified.length + ' modified classes');
-    for (let i = 0; i < modified.length; i++) {
-        fs.copySync(path.join(modifiedDir, modified[i]), path.join(outDir, modified[i]));
-    }
+  const modified = chemlibClasses.modified;
+  log(`Copying ${modified.length} modified classes`);
+  for (let i = 0; i < modified.length; i++) {
+    fs.copySync(path.join(modifiedDir, modified[i]), path.join(outDir, modified[i]));
+  }
 
-    const changed = chemlibClasses.changed;
-    for (let i = 0; i < changed.length; i++) {
-        const file = changed[i][0];
-        const callback = changed[i][1];
-        const data = fs.readFileSync(path.join(outDir, file), 'utf8');
-        const result = callback(data);
-        fs.writeFileSync(path.join(outDir, file), result);
-    }
+  const changed = chemlibClasses.changed;
+  for (let i = 0; i < changed.length; i++) {
+    const file = changed[i][0];
+    const callback = changed[i][1];
+    const data = fs.readFileSync(path.join(outDir, file), 'utf8');
+    const result = callback(data);
+    fs.writeFileSync(path.join(outDir, file), result);
+  }
 
-    const removed = chemlibClasses.removed;
-    for (const removedFile of removed) {
-        fs.removeSync(path.join(outDir, removedFile));
-    }
+  const removed = chemlibClasses.removed;
+  for (const removedFile of removed) {
+    fs.removeSync(path.join(outDir, removedFile));
+  }
 }
 
 function log(value) {
-    if (verbose) {
-        console.log(value);
-    }
+  if (verbose) {
+    // eslint-disable-next-line no-console
+    console.log(value);
+  }
 }
--- openchemlib/classes.js
+++ openchemlib/classes.js
@@ -1,91 +1,93 @@
 'use strict';
 
 const modified = [
-    'calc/ArrayUtilsCalc',
+  'calc/ArrayUtilsCalc',
 
-    'chem/AbstractDrawingObject',
-    'chem/DepictorTransformation',
+  'chem/AbstractDrawingObject',
+  'chem/DepictorTransformation',
 
-    'chem/prediction/DruglikenessPredictor',
-    'chem/prediction/IncrementTable',
-    'chem/prediction/ToxicityPredictor',
+  'chem/prediction/DruglikenessPredictor',
+  'chem/prediction/IncrementTable',
+  'chem/prediction/ToxicityPredictor',
 
-    'util/ConstantsDWAR'
+  'util/ConstantsDWAR'
 ];
 
 exports.modified = modified.map(getFilename);
 
 const changed = [
-    ['chem/ChemistryHelper', removePrintf],
-    ['chem/Molecule', changeMolecule],
-    ['share/gui/editor/Model', removePrintf],
-    ['util/ArrayUtils', changeArrayUtils]
+  ['chem/ChemistryHelper', removePrintf],
+  ['chem/Molecule', changeMolecule],
+  ['share/gui/editor/Model', removePrintf],
+  ['util/ArrayUtils', changeArrayUtils]
 ];
 
 exports.changed = changed.map((file) => {
-    return [
-        getFilename(file[0]),
-        file[1]
-    ];
+  return [
+    getFilename(file[0]),
+    file[1]
+  ];
 });
 
 const removed = [
-    'calc/statistics/median/ModelMedianDouble.java', // uses StringFunctions
-    'chem/dnd', // ui
-    'chem/FingerPrintGenerator.java',
-    'chem/io/DWARFileParser.java',
-    'chem/reaction/ClassificationData.java',
-    'gui/dnd', // ui
-    'gui/hidpi', // ui
-    'share/gui/editor/chem/DrawingObject.java',
-    // 'util/ArrayUtils.java', // uses reflection
-    'util/CursorHelper.java',
-    'util/datamodel/IntVec.java',
-    'util/IntQueue.java', // unused, depends on ArrayUtils
-    'util/Platform.java',
-    'util/StringFunctions.java' // uses RegExp things
+  'calc/statistics/median/ModelMedianDouble.java', // uses StringFunctions
+  'chem/dnd', // ui
+  'chem/FingerPrintGenerator.java',
+  'chem/io/DWARFileParser.java',
+  'chem/reaction/ClassificationData.java',
+  'gui/dnd', // ui
+  'gui/hidpi', // ui
+  'share/gui/editor/chem/DrawingObject.java',
+  // 'util/ArrayUtils.java', // uses reflection
+  'util/CursorHelper.java',
+  'util/datamodel/IntVec.java',
+  'util/IntQueue.java', // unused, depends on ArrayUtils
+  'util/Platform.java',
+  'util/StringFunctions.java' // uses RegExp things
 ];
 
 exports.removed = removed.map(getFolderName);
 
 function getFilename(file) {
-    return 'actelion/research/' + file + '.java';
+  return `actelion/research/${file}.java`;
 }
 
 function getFolderName(file) {
-    return 'actelion/research/' + file;
+  return `actelion/research/${file}`;
 }
 
 function changeMolecule(molecule) {
-    molecule = molecule.replace('import java.lang.reflect.Array;\n', '');
-    const copyOf = 'protected final static Object copyOf';
-    const copyOfIndex = molecule.indexOf(copyOf);
-    if (copyOfIndex === -1) throw new Error('did not find copyOf method');
-    const closeIndex = molecule.indexOf('}', copyOfIndex);
-    molecule = molecule.substr(0, closeIndex + 1) + '*/' + molecule.substr(closeIndex + 1);
-    molecule = molecule.substr(0, copyOfIndex) + '/*' + molecule.substr(copyOfIndex);
-    molecule = molecule.replace(/\([^)]+\)copyOf/g, 'Arrays.copyOf');
-    return molecule;
+  molecule = molecule.replace('import java.lang.reflect.Array;\n', '');
+  const copyOf = 'protected final static Object copyOf';
+  const copyOfIndex = molecule.indexOf(copyOf);
+  if (copyOfIndex === -1) throw new Error('did not find copyOf method');
+  const closeIndex = molecule.indexOf('}', copyOfIndex);
+  molecule = `${molecule.substr(0, closeIndex + 1)}*/${molecule.substr(closeIndex + 1)}`;
+  molecule = `${molecule.substr(0, copyOfIndex)}/*${molecule.substr(copyOfIndex)}`;
+  molecule = molecule.replace(/\([^)]+\)copyOf/g, 'Arrays.copyOf');
+  return molecule;
 }
 
 function removePrintf(code) {
-    return code.replace(/System\.out\.printf/g, '// System.out.print');
+  return code.replace(/System\.out\.printf/g, '// System.out.print');
 }
 
 function changeArrayUtils(code) {
-    code = removeSlice(code, '\n	/**\n	 * Resize an array', 'return newArray;\n	}');
-    code = removeSlice(code, '\n	/**\n	 * Copy an array ', 'return newArray;\n	}');
-    return code;
+  // eslint-disable-next-line no-tabs
+  code = removeSlice(code, '\n	/**\n	 * Resize an array', 'return newArray;\n	}');
+  // eslint-disable-next-line no-tabs
+  code = removeSlice(code, '\n	/**\n	 * Copy an array ', 'return newArray;\n	}');
+  return code;
 }
 
 function removeSlice(code, start, end) {
-    const startIdx = code.indexOf(start);
-    if (startIdx === -1) {
-        throw new Error('did not find index for: ' + start);
-    }
-    const endIdx = code.indexOf(end, startIdx + start.length);
-    if (endIdx === -1) {
-        throw new Error('did not find index for: ' + end);
-    }
-    return code.substr(0, startIdx) + code.substr(endIdx + end.length);
+  const startIdx = code.indexOf(start);
+  if (startIdx === -1) {
+    throw new Error(`did not find index for: ${start}`);
+  }
+  const endIdx = code.indexOf(end, startIdx + start.length);
+  if (endIdx === -1) {
+    throw new Error(`did not find index for: ${end}`);
+  }
+  return code.substr(0, startIdx) + code.substr(endIdx + end.length);
 }
