3f8ea887a97cbc8f59c7b7d3f9118fa6399fc512
Luc Patiny <luc@patiny.com>
2022-10-17 07:57:11 +0200

    chore: add hacked molfile example

--- examples/v3000/load.js
+++ examples/v3000/load.js
@@ -9,6 +9,16 @@ const molfile = readFileSync(join(__dirname, 'molfile.txt'), 'utf8');
 const molecule = Molecule.fromMolfile(molfile);
 console.log('molecule', molecule.getAllAtoms());
 
+const molfileHacked = readFileSync(
+  join(__dirname, 'molfile.txt'),
+  'utf8',
+).replace(
+  /M {2}V30 BEGIN SGROUP[\r\n]+(.*[\r\n]+)*M {2}V30 END SGROUP[\r\n]+/,
+  '',
+);
+const moleculeHacked = Molecule.fromMolfile(molfileHacked);
+console.log('moleculeHacked', moleculeHacked.getAllAtoms());
+
 const molfileNoSGroup = readFileSync(
   join(__dirname, 'molfileNoSGroup.txt'),
   'utf8',
