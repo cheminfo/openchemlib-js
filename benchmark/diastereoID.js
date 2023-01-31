ec25bec755ffd7836a51a97bfb36f18a38530b02
Luc Patiny <luc@patiny.com>
2023-01-31 09:39:49 +0100

    chore: improve benchmark

--- benchmark/diastereoID.js
+++ benchmark/diastereoID.js
@@ -2,16 +2,17 @@
 
 const { getDiastereotopicAtomIDs } = require('openchemlib-utils');
 
-const { Molecule } = require('../dist/openchemlib-full.pretty');
+const { Molecule, version } = require('../dist/openchemlib-full');
 
 async function doAll() {
+  console.log('OCL version', version);
   console.time('diastereoID');
   const response = await fetch(
     'https://wikipedia.cheminfo.org/src/json/data.json',
   );
   const entries = (await response.json()).data.molecules;
   let totalLength = 0;
-  for (const entry of entries.slice(0, 200)) {
+  for (const entry of entries.slice(0, 700)) {
     const molecule = Molecule.fromIDCode(entry.actID.value);
     const ids = getDiastereotopicAtomIDs(molecule);
     totalLength += ids.length;
--- benchmark/wikipedia.js
+++ benchmark/wikipedia.js
@@ -1,15 +1,16 @@
 'use strict';
 
-const { Molecule } = require('../dist/openchemlib-full.pretty');
+const { Molecule, version } = require('../dist/openchemlib-full');
 
 async function doAll() {
+  console.log('OCL version', version);
   console.time('wikipedia');
   const response = await fetch(
     'https://wikipedia.cheminfo.org/src/json/data.json',
   );
   const entries = (await response.json()).data.molecules;
   const idCodes = [];
-  for (const entry of entries.slice(0, 2000)) {
+  for (const entry of entries.slice(0, 10000)) {
     const molecule = Molecule.fromIDCode(entry.actID.value);
     const idCode = molecule.getIDCode();
     idCodes.push(idCode);
