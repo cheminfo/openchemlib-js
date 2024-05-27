f9d9cb0e02c60b843e886c7431fd8e0b75a9fdf6
Luc Patiny <luc@patiny.com>
2024-05-27 11:23:38 +0200

    feat: add method Molecule.getAtomQueryFeaturesObject

--- __tests__/molecule.queryFeatures.test.js
+++ __tests__/molecule.queryFeatures.test.js
@@ -2,8 +2,22 @@
 
 const { Molecule } = require('../minimal');
 
-test('queryFeatures', () => {
-  const molecule = Molecule.fromSmiles('CCO');
-  const features = molecule.getAtomQueryFeaturesObject(0);
-  console.log(features);
+test('queryFeatures more than 2 neighbours', () => {
+  const molecule = Molecule.fromIDCode('eF@Hp[qp');
+
+  const firstQueryFeatures = molecule.getAtomQueryFeaturesObject(0);
+  expect(firstQueryFeatures.not0Neighbours).toBe(true);
+  expect(firstQueryFeatures.not1Neighbours).toBe(true);
+  expect(firstQueryFeatures.not2Neighbours).toBe(true);
+  expect(firstQueryFeatures.not3Neighbours).toBe(false);
+
+});
+
+test('queryFeatures exactly 2 hydrogens', () => {
+  const molecule = Molecule.fromIDCode('eF@Hp_Qh');
+  const firstQueryFeatures = molecule.getAtomQueryFeaturesObject(0);
+  expect(firstQueryFeatures.not0Hydrogen).toBe(true);
+  expect(firstQueryFeatures.not1Hydrogen).toBe(true);
+  expect(firstQueryFeatures.not2Hydrogen).toBe(false);
+  expect(firstQueryFeatures.not3Hydrogen).toBe(true);
 });
