61f17d5b5a774d1220e9eca72a3eec0e011dcd44
Michaël Zasso <targos@protonmail.com>
2024-07-24 11:28:01 +0200

    chore: add TailwindCSS for demo styles

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './examples/**/*.{html,js,ts}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
