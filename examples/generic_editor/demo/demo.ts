85584b75f582179877d7e17e9b09d685f1bc5ee6
tpoisseau <22891227+tpoisseau@users.noreply.github.com>
2024-10-17 10:32:35 +0200

    chore: add ID-Code loader for `/examples/generic_editor/demo.html`

--- examples/generic_editor/demo/demo.ts
+++ examples/generic_editor/demo/demo.ts
@@ -3,7 +3,7 @@ import { getEditor, resetEditor } from './editor.ts';
 import rxn from './seeds/rxn.ts';
 import molfile from './seeds/molfile.ts';
 
-const { Molecule, Reaction } = OCL;
+const { Molecule, Reaction, ReactionEncoder } = OCL;
 
 resetEditor();
 
@@ -49,4 +49,57 @@ loadReactionFragment.onclick = () => {
   const reaction = Reaction.fromRxn(rxn);
   reaction.setFragment(true);
   getEditor().setReaction(reaction);
+};
+
+const idcodeForm = document.getElementById('idcodeForm') as HTMLFormElement;
+idcodeForm.onsubmit = (event) => {
+  event.preventDefault();
+  event.stopPropagation();
+
+  const formData = new FormData(idcodeForm);
+  const idcode = formData.get('idcode') as string;
+  const type = formData.get('type') as string;
+  const ensureCoordinates = formData.has('ensureCoordinates');
+
+  if (type === 'molecule') {
+    const molecule = Molecule.fromIDCode(idcode, ensureCoordinates);
+    getEditor().setMolecule(molecule);
+    return;
+  }
+
+  if (type === 'reaction') {
+    const useMode = formData.has('options-type');
+
+    function getMode() {
+      const mapping = formData.has('INCLUDE_MAPPING')
+        ? ReactionEncoder.INCLUDE_MAPPING
+        : 0;
+      const coords = formData.has('INCLUDE_COORDS')
+        ? ReactionEncoder.INCLUDE_COORDS
+        : 0;
+      const draw = formData.has('INCLUDE_DRAWING_OBJECTS')
+        ? ReactionEncoder.INCLUDE_DRAWING_OBJECTS
+        : 0;
+      const catalysts = formData.has('INCLUDE_CATALYSTS')
+        ? ReactionEncoder.INCLUDE_CATALYSTS
+        : 0;
+
+      return mapping | coords | draw | catalysts;
+    }
+
+    const reaction = useMode
+      ? ReactionEncoder.decode(idcode, {
+          mode: getMode(),
+        })
+      : ReactionEncoder.decode(idcode, {
+          ensureCoordinates: ensureCoordinates,
+        });
+
+    if (!reaction) {
+      window.alert('Invalid reaction');
+      return;
+    }
+
+    getEditor().setReaction(reaction);
+  }
 };
