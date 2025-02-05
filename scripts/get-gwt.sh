fa4032e625220f9d0667aedc30e3888da3c76586
Michaël Zasso <targos@protonmail.com>
2025-02-05 09:48:47 +0100

    feat: update GWT to v2.12.1

--- scripts/get-gwt.sh
+++ scripts/get-gwt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget -q https://github.com/gwtproject/gwt/releases/download/2.11.0/gwt-2.11.0.zip -O gwt.zip
+wget -q https://github.com/gwtproject/gwt/releases/download/2.12.1/gwt-2.12.1.zip -O gwt.zip
 unzip -q gwt.zip
 mv gwt-2* gwt
 echo '{"gwt": "./gwt"}' > config.json
