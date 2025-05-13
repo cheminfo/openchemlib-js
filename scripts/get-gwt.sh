de2c4970d5652d454b5130040acffae70ebbde4a
tpoisseau <22891227+tpoisseau@users.noreply.github.com>
2025-05-13 09:54:19 +0200

    fix: use head prepend instead mutate possibly external stylesheet
    Closes: https://github.com/cheminfo/openchemlib-js/issues/288

--- scripts/get-gwt.sh
+++ scripts/get-gwt.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# clear previous gwt installation
+rm -rf gwt
+
 wget -q https://github.com/gwtproject/gwt/releases/download/2.12.2/gwt-2.12.2.zip -O gwt.zip
 unzip -q gwt.zip
 mv gwt-2* gwt
--- scripts/get-java.sh
+++ scripts/get-java.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# clear previous jdk installation
+rm -rf jdk
+
 wget -q https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.6%2B7/OpenJDK21U-jdk_x64_linux_hotspot_21.0.6_7.tar.gz -O jdk.tar.gz
 tar xf jdk.tar.gz
 mv jdk-21* jdk
