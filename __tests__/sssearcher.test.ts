290d818f17a121f11fa1ac6e7a152a921629104c
Luc Patiny <luc@patiny.com>
2025-10-15 08:17:20 +0200

    test: add test cases for new sssearcher options

--- __tests__/sssearcher.test.ts
+++ __tests__/sssearcher.test.ts
@@ -38,31 +38,79 @@ describe('SSSearcher', () => {
   });
 
   it('find all matches', () => {
-    const searcher = new SSSearcher();
-    const diketone = Molecule.fromSmiles('CCC(=O)CCC(=O)CC');
-    const ketoneFragment = Molecule.fromSmiles('C(=O)');
-    ketoneFragment.setFragment(true);
-    searcher.setMolecule(diketone);
-    searcher.setFragment(ketoneFragment);
-    searcher.findFragmentInMolecule();
-    const matchList = searcher.getMatchList();
-    console.log(matchList);
-    expect(matchList.length).toBe(2);
+    const scn = Molecule.fromSmiles('[15N]#C=[S-]');
+    const nitrogen = Molecule.fromSmiles('N');
+    nitrogen.setFragment(true);
+    const sulfur = Molecule.fromSmiles('S');
+    sulfur.setFragment(true);
+
+    let searcher = new SSSearcher({});
+    searcher.setMolecule(scn);
+    searcher.setFragment(nitrogen);
+    expect(searcher.findFragmentInMolecule()).toBe(1);
+
+    // need to match charge
+    searcher = new SSSearcher({ matchAtomCharge: true });
+    searcher.setMolecule(scn);
+    searcher.setFragment(nitrogen);
+    expect(searcher.findFragmentInMolecule()).toBe(1);
+    searcher.setFragment(sulfur);
+    expect(searcher.findFragmentInMolecule()).toBe(0);
+
+    // need to match mass
+    searcher = new SSSearcher({ matchAtomMass: true });
+    searcher.setMolecule(scn);
+    searcher.setFragment(nitrogen);
+    expect(searcher.findFragmentInMolecule()).toBe(0);
+    searcher.setFragment(sulfur);
+    expect(searcher.findFragmentInMolecule()).toBe(1);
+
+    // need to match charge and mass, check if combination works correctly
+    searcher = new SSSearcher({ matchAtomMass: true, matchAtomCharge: true });
+    searcher.setMolecule(scn);
+    searcher.setFragment(nitrogen);
+    expect(searcher.findFragmentInMolecule()).toBe(0);
+    searcher.setFragment(sulfur);
+    expect(searcher.findFragmentInMolecule()).toBe(0);
   });
 
-  it.only('check mode', () => {
-    const searcher = new SSSearcher(0);
-    const diketone = Molecule.fromSmiles('CCC(=O)CCC(=O)CC');
-    const ketoneFragment = Molecule.fromSmiles('C(=O)');
-    ketoneFragment.setFragment(true);
+  it('check mode', () => {
+    const searcher = new SSSearcher();
+    const diketone = Molecule.fromSmiles('CCC(=O)C(=O)CC');
+    const carbonyFragment = Molecule.fromSmiles('C(=O)');
+    carbonyFragment.setFragment(true);
     searcher.setMolecule(diketone);
-    searcher.setFragment(ketoneFragment);
-    const number = searcher.findFragmentInMolecule();
-    console.log(number);
+    searcher.setFragment(carbonyFragment);
+    const found = searcher.findFragmentInMolecule();
+    expect(found).toBe(2);
     const matchList = searcher.getMatchList();
-    console.log(matchList);
-    console.log(matchList.size());
-    return;
-    expect(matchList.length).toBe(2);
+    expect(matchList).toStrictEqual([
+      [2, 3],
+      [4, 5],
+    ]);
+    const foundExistence = searcher.findFragmentInMolecule({
+      countMode: 'existence',
+    });
+    expect(foundExistence).toBe(1);
+    const foundOverlapping = searcher.findFragmentInMolecule({
+      countMode: 'overlapping',
+    });
+    expect(foundOverlapping).toBe(2);
+    const foundSeparated = searcher.findFragmentInMolecule({
+      countMode: 'separated',
+    });
+    expect(foundSeparated).toBe(2);
+
+    const ketoneFragment = Molecule.fromSmiles('CC(=O)C');
+    ketoneFragment.setFragment(true);
+    searcher.setFragment(ketoneFragment);
+    const foundKetoneOverlapping = searcher.findFragmentInMolecule({
+      countMode: 'overlapping',
+    });
+    expect(foundKetoneOverlapping).toBe(2);
+    const foundKetoneSeparated = searcher.findFragmentInMolecule({
+      countMode: 'separated',
+    });
+    expect(foundKetoneSeparated).toBe(1);
   });
 });
